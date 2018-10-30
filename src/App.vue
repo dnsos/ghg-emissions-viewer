c2c810b1f27d9c96384f20fd298e64d967d0db3b
Dennis <dennisostendorf@web.de>
2018-10-30 17:50:16 +0200

    Added more data (and various tests)

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,58 @@
 <style>
 :root {
   --primary-color: #003399;
-  --neutral-color: rgb(40, 40, 40);
-  --font-size: 16px;
-  --font-family: "Open Sans", sans-serif;
+  --neutral-color: #3e3e3e;
+  --medium-color: #8c8c8c;
+  --light-color: #efefef;
+  --superlight-color: #f9f9f9;
+  --font-size: 15px;
+  --font-family: "Fira Sans", sans-serif;
+  --spacing: 2.5rem;
+}
+
+html {
+  font-size: 62.5%;
 }
 
 body {
   padding: 0;
   margin: 0;
   color: var(--neutral-color);
+  background-color: var(--superlight-color);
   font-family: var(--font-family);
   font-size: var(--font-size);
 }
+
+#app {
+  padding: var(--spacing);
+}
+
+h1, h2, h3, h4, h5, h6 {
+  margin-top: 0;
+  margin-bottom: var(--spacing);
+  font-weight: 300; }
+h1 { font-size: 4.0rem; line-height: 1.2;  letter-spacing: -.1rem;}
+h2 { font-size: 3.6rem; line-height: 1.25; letter-spacing: -.1rem; }
+h3 { font-size: 3.0rem; line-height: 1.3;  letter-spacing: -.1rem; }
+h4 { font-size: 2.4rem; line-height: 1.35; letter-spacing: -.08rem; }
+h5 { font-size: 1.8rem; line-height: 1.5;  letter-spacing: -.05rem; }
+h6 { font-size: 1.5rem; line-height: 1.6;  letter-spacing: 0; }
+
+@media (min-width: 550px) {
+  h1 { font-size: 5.0rem; }
+  h2 { font-size: 4.2rem; }
+  h3 { font-size: 3.6rem; }
+  h4 { font-size: 3.0rem; }
+  h5 { font-size: 2.4rem; }
+  h6 { font-size: 1.5rem; }
+}
+
+p {
+  margin-top: 0;
+  margin-bottom: var(--spacing); }
+
+a {
+  color: #1EAEDB; }
+a:hover {
+  color: #0FA0CE; }
 </style>
--- src/components/EmissionsGradient.vue
+++ src/components/EmissionsGradient.vue
@@ -1,22 +1,35 @@
 <template>
-  <section class="wrapper" :id="'wrapper__' + country.toLowerCase()">
-    <h3>{{country}}</h3>
+  <section 
+    class="wrapper"
+    :class="{ active: isActive }"
+    :id="'wrapper__' + country.toLowerCase()"
+    @mouseout="isActive = !isActive"
+    @mouseover="isActive = !isActive"
+  >
+    <h4>{{country}}</h4>
     <!-- SVG ELement for Emissions Gradient -->
-    <svg :width="gradientWidth" :height="gradientHeight">
-      <g>
-        <!-- append cells as svg rects -->
-        <rect
-          v-for="(value, index) in values"
-          :key="index"
-          :x="cellWidth * index"
-          :y="0"
-          :width="cellWidth"
-          :height="gradientHeight"
-          :fill="cellColor(value)"
-          >
-        </rect>
-      </g>
-    </svg>
+    <figure :class="{ active: isActive }">
+      <svg :width="gradientWidth" :height="gradientHeight">
+        <g>
+          <!-- append cells as svg rects -->
+          <rect
+            v-for="(value, index) in values"
+            :key="index"
+            :x="cellWidth * index"
+            :y="0"
+            :width="cellWidth"
+            :height="gradientHeight"
+            :fill="cellColor(value)"
+            @mouseover="logValues(value, index, intervalStart)"
+            >
+          </rect>
+        </g>
+      </svg>
+    </figure>
+    <!--<label class="color-checkbox" v-show="isActive">
+      <input type="checkbox">
+      Show absolute cell colors
+    </label>-->
   </section>
 </template>
 
@@ -25,28 +38,33 @@ import chroma from 'chroma-js' // chroma for color scales
 
 export default {
   name: 'EmissionsGradient',
-  props: ['country', 'values'],
-  data: function() {
+  props: ['intervalStart', 'country', 'values', 'initialValue'],
+  data: function () {
     // define svg settings
     return {
-      gradientWidth: 300,
+      isActive: false,
+      gradientWidth: 412,
       gradientHeight: 20
     }
   },
   computed: {
-    cellWidth: function() { // compute width of cell
+    cellWidth: function () { // compute width of cell
       return this.gradientWidth / this.values.length
     },
-    cellColor: function(value) { // compute color of cell using chroma.js
-      return chroma.scale(['white', 'black']).domain([0, 100])
+    cellColor: function (value) { // compute color of cell using chroma.js
+      return chroma.scale(['white', 'black']).domain([2286, 1263708])
     }
   },
   methods: {
-    handleResize() {
-      this.gradientWidth = document.querySelector('.wrapper').offsetWidth
+    handleResize () {
+      this.gradientWidth = document.querySelector('.wrapper').offsetWidth - 50
+    },
+    logValues (value, index, intervalStart) {
+      console.log(intervalStart + index, value)
+      console.log('Initial value for', this.country, 'is:', this.initialValue)
     }
   },
-  created: function() {
+  created: function () {
     window.addEventListener('resize', this.handleResize)
     this.handleResize()
   }
@@ -55,7 +73,38 @@ export default {
 
 <style scoped>
   .wrapper {
-    line-height: 1;
-    border: 1px solid rgb(220, 220, 220);
+    padding: var(--spacing);
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-areas:
+    "h h h h h h h h h h h h"
+    "g g g g g g g g g g g g"
+    "c c c c c c c c c c c c"
+    ;
+    background-color: white;
+  }
+
+  h4 {
+    grid-area: h;
+    margin-bottom: calc(var(--spacing) - 2rem);
+  }
+
+  figure {
+    grid-area: g;
+    width: 100%;
+    line-height: 0;
+    padding: 0;
+    margin: 0;
+    border: 1px solid transparent;
+    overflow: hidden;
+  }
+
+  .color-checkbox {
+    grid-area: c;
+  }
+
+  section.active {
+     box-shadow: 0px 0px 10px var(--light-color);
   }
 </style>
